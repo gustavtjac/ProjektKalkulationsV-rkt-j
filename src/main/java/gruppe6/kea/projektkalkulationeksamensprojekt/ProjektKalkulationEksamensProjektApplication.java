7a433a8702738f4ae4827477e854daa3da6a2e83
gustavtjac <gustavtorup@gmail.com>
2025-04-25 11:44:42 +0200

    init commit

---
+++ src/main/java/gruppe6/kea/projektkalkulationeksamensprojekt/ProjektKalkulationEksamensProjektApplication.java
@@ -0,0 +1,13 @@
+package gruppe6.kea.projektkalkulationeksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKalkulationEksamensProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKalkulationEksamensProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe6/kea/projektkalkulationeksamensprojekt/ProjektKalkulationEksamensProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe6.kea.projektkalkulationeksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKalkulationEksamensProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
