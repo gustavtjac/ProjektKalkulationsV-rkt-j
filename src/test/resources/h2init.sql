5930b7c0ebb18610d97cabe29c60f217856a89e6
gustavtjac <gustavtorup@gmail.com>
2025-05-13 09:56:38 +0200

    klar til test

---
+++ src/test/resources/h2init.sql
@@ -0,0 +1,78 @@
+DROP DATABASE IF EXISTS ProjectManagementDB;
+CREATE DATABASE ProjectManagementDB;
+USE ProjectManagementDB;
+
+-- Opretter Profile tabel
+CREATE TABLE Profile (
+                         PROFILE_USERNAME VARCHAR(25) PRIMARY KEY,
+                         PROFILE_NAME VARCHAR(30),
+                         PROFILE_PASSWORD VARCHAR(60) NOT NULL,
+                         PROFILE_AUTH_CODE INTEGER(1) NOT NULL,
+                         PROFILE_SALARY DOUBLE(6,2) not null
+);
+
+CREATE TABLE Project (
+                         PROJECT_ID CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+                         PROJECT_OWNER_PROFILE_USERNAME VARCHAR(25) NOT NULL,
+                         PROJECT_NAME VARCHAR(50) NOT NULL,
+                         PROJECT_DESC VARCHAR(500),
+                         PROJECT_MAX_TIME DOUBLE(30,2) NOT NULL,
+                         PROJECT_MAX_PRICE DOUBLE(30,2) NOT NULL,
+                         PROJECT_ENDDATE DATE not null,
+                         FOREIGN KEY (PROJECT_OWNER_PROFILE_USERNAME) REFERENCES Profile(PROFILE_USERNAME) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Opretter Task tabel
+CREATE TABLE Task (
+                      TASK_ID CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+                      TASK_PROJECT_ID CHAR(36) NOT NULL,
+                      TASK_NAME VARCHAR(50) NOT NULL,
+                      TASK_DESC VARCHAR(500),
+                      TASK_MAX_TIME DOUBLE(30,2) NOT NULL,
+                      TASK_MAX_PRICE DOUBLE(30,2) NOT NULL,
+                      FOREIGN KEY (TASK_PROJECT_ID) REFERENCES Project(PROJECT_ID) ON DELETE CASCADE
+);
+
+-- Opretter Subtask tabel
+CREATE TABLE Subtask (
+                         SUBTASK_ID CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+                         SUBTASK_TASK_ID CHAR(36) NOT NULL,
+                         SUBTASK_NAME VARCHAR(50) NOT NULL,
+                         SUBTASK_DESC VARCHAR(500),
+                         SUBTASK_TIME DOUBLE(30,2) NOT NULL,
+                         SUBTASK_STATUS integer(1) not null default (1),
+                         FOREIGN KEY (SUBTASK_TASK_ID) REFERENCES Task(TASK_ID) ON DELETE CASCADE
+);
+
+-- Opretter  Skill tabel
+CREATE TABLE Skill (
+                       SKILL_ID CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+                       SKILL_NAME VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- Opretter  Profile_Skill tabel
+CREATE TABLE Profile_Skill (
+                               PROFILE_USERNAME VARCHAR(25) NOT NULL,
+                               SKILL_ID CHAR(36) NOT NULL,
+                               PRIMARY KEY (PROFILE_USERNAME, SKILL_ID),
+                               FOREIGN KEY (PROFILE_USERNAME) REFERENCES Profile(PROFILE_USERNAME) ON DELETE CASCADE ON UPDATE CASCADE,
+                               FOREIGN KEY (SKILL_ID) REFERENCES Skill(SKILL_ID) ON DELETE CASCADE
+);
+
+-- Opretter  Profile_Project tabel
+CREATE TABLE Profile_Project (
+                                 PROJECT_ID CHAR(36) NOT NULL,
+                                 PROFILE_USERNAME VARCHAR(25) NOT NULL,
+                                 PRIMARY KEY (PROFILE_USERNAME, PROJECT_ID),
+                                 FOREIGN KEY (PROFILE_USERNAME) REFERENCES Profile(PROFILE_USERNAME) ON DELETE CASCADE ON UPDATE CASCADE,
+                                 FOREIGN KEY (PROJECT_ID) REFERENCES Project(PROJECT_ID) ON DELETE CASCADE
+);
+
+-- Opretter Subtask_Profile tabel
+CREATE TABLE Subtask_Profile (
+                                 SUBTASK_ID CHAR(36) NOT NULL,
+                                 PROFILE_USERNAME VARCHAR(25) NOT NULL,
+                                 PRIMARY KEY (PROFILE_USERNAME, SUBTASK_ID),
+                                 FOREIGN KEY (PROFILE_USERNAME) REFERENCES Profile(PROFILE_USERNAME) ON DELETE CASCADE ON UPDATE CASCADE,
+                                 FOREIGN KEY (SUBTASK_ID) REFERENCES Subtask(SUBTASK_ID) ON DELETE CASCADE
+);
